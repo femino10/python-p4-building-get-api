c56f4ac14dd592aa623a34767394a2e1a5b6c97e
femino10 <your-github-username@users.noreply.github.com>
2024-01-29 15:40:05 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
-from models import db, User, Review, Game
+from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -17,7 +16,59 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return "Index for Game/Review/User API"
+    return '<h1>Bakery GET API</h1>'
+
+@app.route('/bakeries')
+def bakeries():
+    query = Bakery.query.all()
+    bakeries = []
+    for bakery in query:
+        bakery_dict = {
+            'id': bakery.id,
+            'name': bakery.name,
+            'created_at': bakery.created_at
+        }
+        bakeries.append(bakery_dict)
+    response = make_response(
+        bakeries, 200
+    )
+    return response
+
+@app.route('/bakeries/<int:id>')
+def bakery_by_id(id):
+    query = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = {
+            'id': query.id,
+            'name': query.name,
+            'created_at': query.created_at
+        }
+    response = make_response(jsonify(bakery_dict), 200)
+    return response
+
+@app.route('/baked_goods/by_price')
+def baked_goods_by_price():
+    query = BakedGood.query.order_by((BakedGood.price).desc())
+    listofbakedgoods = []
+    for bakedgood in query:
+        dict = {
+            'id': bakedgood.id,
+            'name': bakedgood.name,
+            'price': bakedgood.price,
+            'created_at': bakedgood.created_at
+            }
+        listofbakedgoods.append(dict)
+    return listofbakedgoods
+
+@app.route('/baked_goods/most_expensive')
+def most_expensive_baked_good():
+    query = BakedGood.query.order_by((BakedGood.price).desc()).first()
+    dict = {
+            'id': query.id,
+            'name': query.name,
+            'price': query.price,
+            'created_at': query.created_at
+            }
+    return make_response(dict, 200)
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=555, debug=True)
